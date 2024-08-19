eceabdb1c6fcf8ec98d11c1bb3cdca7902994d28
MaryKwawu <may419316@gmail.com>
2024-08-19 15:55:52 +0000

    Hot fix : re-named a place holder

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -230,7 +230,7 @@ const AboutMe = () => {
 							<div className="flex flex-col gap-[30px] text-center xl:text-left">
 								<h3 className="text-4xl font-bold">{experience.title}</h3>
 								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-									{experience.description}
+									{experience.Description}
 								</p>
 								<ScrollArea className="h-[400px]">
 									<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
